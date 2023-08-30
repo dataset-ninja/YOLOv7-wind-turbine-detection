b809446ab8339b2da8b9a893dec27bc0e0d4a6ac
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-08-30 17:46:06 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -35,7 +35,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://github.com/nvriese1/WindTurbineDetection"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 1846409
+PREVIEW_IMAGE_ID: int = 1844190
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/YOLOv7-wind-turbine-detection"
