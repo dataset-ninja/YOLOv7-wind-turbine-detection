622c7ca88c05059c73c795e54638bde6be17887e
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-10-16 18:55:00 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -21,7 +21,7 @@ HIDE_DATASET = False  # set False when 100% sure about repo quality
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = License.MIT()
+LICENSE: License = License.MIT(source_url="https://github.com/nvriese1/WindTurbineDetection/blob/main/LICENSE")
 APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Energy(is_used=False)]
 CATEGORY: Category = Category.EnergyAndUtilities(extra=[Category.Aerial(), Category.Satellite()])
 
